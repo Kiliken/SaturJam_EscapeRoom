2441ce591b95a826026649cbd4d07af9b0238aef
Kiliken <francescopaolomariani7@gmail.com>
2025-08-23 12:57:56 +0900

    Unity Dynamic Log

--- RunUnity.bat
+++ RunUnity.bat
@@ -4,4 +4,8 @@ if exist "C:\Program Files\Unity\Hub\Editor\2022.3.56f1\Editor\Unity.exe" set un
 if exist "D:\Program Files\Unity\Editor\2022.3.56f1\Editor\Unity.exe" set unityPath="D:\Program Files\Unity\Editor\2022.3.56f1\Editor\Unity.exe"
 
 
-%unityPath% -projectPath %cd%
+start "" %unityPath% -projectPath %cd% -logFile "%cd%\Logs\CurrentLogs.log" 
+
+powershell -Command "Get-Content 'Logs\CurrentLogs.log' -Wait"
+
+pause
